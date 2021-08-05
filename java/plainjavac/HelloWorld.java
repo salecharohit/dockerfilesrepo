278353162c8b67345562b6304893a36ce33f677e
salecharohit <i@rohitsalecha.com>
2021-08-05 13:11:05 +0530

    - running java workflow

--- java/plainjavac/HelloWorld.java
+++ java/plainjavac/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java running on Docker on Alpine OS !");
+        System.out.println("Hello World from Java running on Docker on Alpine OS!");
     }
 }
